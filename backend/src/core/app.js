7e73b19356ec044f5ee9886dac7ad3d5edc83b0a
bestrui <bestrui@bestrui.top>
2026-02-04 11:10:54 +0800

    feat: :sparkles: 同步substore接口

--- backend/src/core/app.js
+++ backend/src/core/app.js
@@ -1,12 +1,12 @@
 const $ = {
     log(msg) {
-        console.log(`SubConv LOG   : ${msg}`);
+        // console.log(`SubConv LOG   : ${msg}`);
     },
     info(msg) {
-        console.log(`SubConv INFO  : ${msg}`);
+        // console.log(`SubConv INFO  : ${msg}`);
     },
     error(msg) {
-        console.log(`SubConv ERROR : ${msg}`);
+        // console.log(`SubConv ERROR : ${msg}`);
     },
     wait(millisec) {
         return new Promise((resolve) => setTimeout(resolve, millisec));
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -27,4 +27,51 @@ app.get("/:token/sub", async (c) => {
   return c.text(result);
 });
 
+app.post("/:token/sub", async (c) => {
+  const token = c.req.param("token");
+  if (c.env.SECRET && c.env.SECRET !== token) {
+    return c.notFound();
+  }
+
+  const body = await c.req.json();
+  const { urls, client } = body as { urls: string[], client: string };
+
+  const mid = (
+    await Promise.all(
+      urls.map(async (u) => {
+        const res = await fetch(u);
+        const text = await res.text();
+        return ProxyUtils.parse(text);
+      })
+    )
+  ).flat();
+
+  var result: any = {};
+  const par_res = ProxyUtils.produce(mid, client as string);
+  result['par_res'] = par_res;
+  return c.json({
+    status: 'success',
+    data: result,
+  }, 200);
+});
+
+app.post("/:token/api/proxy/parse", async (c) => {
+  const token = c.req.param("token");
+  if (c.env.SECRET && c.env.SECRET !== token) {
+    return c.notFound();
+  }
+  const body = await c.req.json();
+  const { data, client } = body;
+  const proxies = ProxyUtils.parse(data);
+  var result: any = {};
+  const par_res = ProxyUtils.produce(proxies, client as string);
+  result['par_res'] = par_res;
+  return c.json({
+    status: 'success',
+    data: result,
+  }, 200);
+});
+
+
+
 export default app;
